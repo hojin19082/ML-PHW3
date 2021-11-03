daabc517db24473b24ce5aada4d63fd5dfa98fa2
changmeen <fbckdals99@gmail.com>
2021-11-03 22:19:45 +0900

    inverted index를 만들고 이를 통해 각 쿼리와 관련된 문서들이 뭐고 개수가 몇개인지 까지 구함

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import pandas as pd
-from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.metrics.pairwise import cosine_similarity
 from nltk.corpus import stopwords
 from nltk import word_tokenize
 from nltk.stem.porter import PorterStemmer
 import re
+import numpy as np
 import nltk
 import itertools
-from sklearn.metrics.pairwise import cosine_similarity
+
 """
 nltk.download('stopwords')
 nltk.download('punkt')
@@ -15,13 +17,13 @@ Stop_Words = stopwords.words("english")
 
 
 def tokenize(text):
-    clean_txt = re.sub('[^a-z\s]+',' ',text)  # replacing spcl chars, punctuations by space
-    clean_txt = re.sub('(\s+)',' ',clean_txt)  # replacing multiple spaces by single space
+    clean_txt = re.sub('[^a-z\s]+', ' ', text)  # replacing spcl chars, punctuations by space
+    clean_txt = re.sub('(\s+)', ' ', clean_txt)  # replacing multiple spaces by single space
     min_length = 3
     words = map(lambda word: word.lower(), word_tokenize(clean_txt))  # tokenizing, lowercase
     words = [word for word in words if word not in Stop_Words]  # filtering stopwords
-    words = filter(lambda t: len(t)>=min_length, words)  # filtering words of length <=2
-    tokens =(list(map(lambda token: PorterStemmer().stem(token),words)))  # stemming tokens
+    words = filter(lambda t: len(t) >= min_length, words)  # filtering words of length <=2
+    tokens =(list(map(lambda token: PorterStemmer().stem(token), words)))  # stemming tokens
     return tokens
 
 
@@ -30,7 +32,7 @@ def processD(article):
     T, _, article = article.partition('\n.A\n')
     A, _, article = article.partition('\n.B\n')
     B, _, W = article.partition('\n.W\n')
-    return {'T':T, 'A':A, 'B':B, 'W':W}
+    return {'T': T, 'A': A, 'B': B, 'W': W}
 
 
 def process(query):
@@ -60,10 +62,10 @@ for iii in key2:
 with open('cran.all.1400.txt') as f:
     articles = f.read().split('\n.I')
 
-data = {(i+1):processD(article) for i,article in enumerate(articles)}
+data = {(i+1): processD(article) for i, article in enumerate(articles)}
 docs = [data[index]['W'] for index in range(1, 1401)]
 
-#각 문서 토큰화 후 list에 저장
+# 각 문서 토큰화 후 list에 저장
 docs_tokens = []
 for i in docs:
     docs_tokens.append(tokenize(i))
@@ -73,7 +75,7 @@ with open('cran.qry.txt') as fq:
     queries = fq.read().split('\n.I')
 
 
-dataq = {(ind+1):process(query) for ind,query in enumerate(queries)}
+dataq = {(ind+1): process(query) for ind, query in enumerate(queries)}
 query = list(dataq.values())
 query_tokens = []
 # 쿼리 token화
@@ -88,4 +90,32 @@ for v in total_query:
         # total_query_tokens에 전체 쿼리에 대한 token이 하나의 list에 들어가 있음.
         total_query_tokens.append(v)
 
-print(total_query_tokens)
+# Inverted Index
+dictionary = {}
+for i in range(len(docs)):
+    check = docs_tokens[i]
+    for item in total_query_tokens:
+        if item in check:
+            if item not in dictionary:
+                dictionary[item] = []
+            if item in dictionary:
+                dictionary[item].append(i+1)
+
+dictionary = sorted(dictionary.items())
+dictionary = dict(dictionary)
+
+i = 0
+for query_token in query_tokens:
+    simi_dict = {}
+    if i == 5:
+        break
+    for token in query_token:
+        value = dictionary[token]
+        for doc_id in value:
+            if doc_id not in simi_dict:
+                simi_dict[doc_id] = 1
+            if doc_id in simi_dict:
+                simi_dict[doc_id] += 1
+
+
+    i += 1
