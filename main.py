dddbd1b092bed3bc6eac2670fd3224cb1d48b49e
changmeen <fbckdals99@gmail.com>
2021-11-04 15:16:45 +0900

    Complete

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 import pandas as pd
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from nltk.corpus import stopwords
 from nltk import word_tokenize
 from nltk.stem.porter import PorterStemmer
-import re
+import re, sys, os, nltk, itertools
+import math
 import numpy as np
-import nltk
-import itertools
 
 """
 nltk.download('stopwords')
@@ -27,7 +25,6 @@ def tokenize(text):
     tokens =(list(map(lambda token: PorterStemmer().stem(token), words)))  # stemming tokens
     return tokens
 
-
 def processD(article):
     article = article.split('\n.T\n')[1]
     T, _, article = article.partition('\n.A\n')
@@ -41,23 +38,20 @@ def process(query):
     W, _, s = query.partition('\n.W\n')
     return W
 
-'''
-keyFile = open('cran.qry.txt', 'r')
-key = keyFile.readlines()
-for ii in key:
-    print(ii+'\n')
 
 responseFile = open('cranqrel.txt', 'r')
 response = responseFile.readlines()
-for i in response:
-    print(i+'\n')
-# query_number(1-225)  document_number  relevance_rank(-1-5)
 
-keyF = open('cran.all.1400.txt', 'r')
-key2 = keyF.readlines()
-for iii in key2:
-    print(iii+'\n')
-'''
+l_l = [response[c].split() for c in range(len(response))]
+lol = [p[:2] for p in l_l]  # lol = listOf [query no. , relevant doc_no.]
+
+qrel_list = []
+for i in range(225):
+    qrel_list.append([])
+    for v in range(len(lol)):
+        if lol[v][0] == str(i+1):
+            qrel_list[i].append(lol[v][1])
+
 
 # 각 문서 나눠서 list에 저장
 with open('cran.all.1400.txt') as f:
@@ -74,7 +68,6 @@ for i in docs:
 # 각 쿼리 나누기
 with open('cran.qry.txt') as fq:
     queries = fq.read().split('\n.I')
-
 
 dataq = {(ind+1): process(query) for ind, query in enumerate(queries)}
 query = list(dataq.values())
@@ -100,45 +93,76 @@ for i in range(len(docs)):
             if item not in dictionary:
                 dictionary[item] = []
             if item in dictionary:
-                dictionary[item].append(i+1)
+                dictionary[item].append(i)
 
 dictionary = sorted(dictionary.items())
 dictionary = dict(dictionary)
 
+vectorizer = TfidfVectorizer(tokenizer=tokenize, use_idf=True, sublinear_tf=True, norm='l2')
 
-vectorizer = CountVectorizer(tokenizer=tokenize, min_df=1,max_df=0.5, ngram_range=(1,2))
-transformer = TfidfTransformer(smooth_idf=False)
+for k_value in range(4, 7):
+    os.system('cls')
+    k = 'k-value' + str(k_value) + ".txt"
+    sys.stdout = open(k, "w")
+    print("When k-value is: " + str(k_value))
+    precision = 0
+    recall = 0
+    i = 0
+    for query_token in query_tokens:
+        print("Query[" + str(i+1) + "]")
+        qvec = query[i]
+        qvec = [qvec]
+        simi_dict = {}
+        for token in query_token:
+            if token in dictionary:
+                value = dictionary[token]
+                for doc_id in value:
+                    if doc_id not in simi_dict:
+                        simi_dict[doc_id] = 1
+                    if doc_id in simi_dict:
+                        simi_dict[doc_id] += 1
 
-i = 0
-for query_token in query_tokens:
-    qvec = query[i]
-    simi_dict = {}
-    if i == 5:
-        break
-    for token in query_token:
-        value = dictionary[token]
-        for doc_id in value:
-            if doc_id not in simi_dict:
-                simi_dict[doc_id] = 1
-            if doc_id in simi_dict:
-                simi_dict[doc_id] += 1
+        doc_list_to_compute_similarity = []
+        for key, value in simi_dict.items():
+            if value >= k_value:
+                doc_list_to_compute_similarity.append(key)
 
-    doc_list_to_compute_similarity = []
-    for key, value in simi_dict.items():
-        if value >= 5:
-            doc_list_to_compute_similarity.append(key)
+        doc_list_to_compute_similarity = sorted(doc_list_to_compute_similarity)
+        if len(doc_list_to_compute_similarity) != 0:
+            temp = list()
+            for item in doc_list_to_compute_similarity:
+                temp.append(docs[item])
 
-    doc_list_to_compute_similarity = sorted(doc_list_to_compute_similarity)
+            tfid_docs = vectorizer.fit_transform(temp)
+            tfid_query = vectorizer.transform(qvec)
 
-    temp= list()
-    for item in doc_list_to_compute_similarity:
-        temp.append(docs[item])
+            similarity = cosine_similarity(tfid_docs, tfid_query)
+            similarity_df = pd.DataFrame(similarity, columns=['cosSim'])
+            similarity_df['Document id'] = \
+                [doc_list_to_compute_similarity[i]+1 for i in range(len(doc_list_to_compute_similarity))]
+            similarity_df.set_index('Document id', inplace=True)
+            similarity_df = similarity_df.sort_values(by=['cosSim'], axis=0,  ascending=False)
+            print(similarity_df.round(5).head(5))
 
-    tfid_docs = vectorizer.fit_transform(temp)
-    qvec = vectorizer.transform(query)
-    tfid_docs_weight = transformer.fit_transform(tfid_docs)
-    qvec_weight = transformer.fit_transform(qvec)
+            list1 = sorted(list(map(int, qrel_list[i])))
+            list2 = [doc_list_to_compute_similarity[i]+1 for i in range(len(doc_list_to_compute_similarity))]
 
-    similarity = cosine_similarity(tfid_docs_weight, qvec_weight)
-    print(similarity)
-    i += 1
+            tp = set(list1) & set(list2)
+            precision += round(len(tp) / len(list2), 2)
+            recall += round(len(tp) / len(list1), 2)
+
+            print("Precision: ", round(len(tp)/len(list2), 2))
+            print("Recall:    ", round(len(tp)/len(list1), 2))
+            print("\n")
+
+        else:
+            print("Precision: ", 0)
+            print("Recall:    ", 0)
+            print("\n")
+
+        i += 1
+
+    print('Mean precision: ', round((precision/225.0), 4))
+    print('Mean recall:    ', round((recall/225.0), 4))
+
+    sys.stdout.close()
